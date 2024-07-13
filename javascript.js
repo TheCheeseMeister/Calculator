8afbf50dea107d24dbf591747b102f6e3d4555f6
TheCheeseMeister <ssomething511@gmail.com>
2024-07-12 23:43:36 -0400

    Final for now; Added keyboard functionality. Wow. Except operators dont totally work so dont use them.

--- javascript.js
+++ javascript.js
@@ -13,6 +13,24 @@ function appendNum(button) {
     else largeText.textContent += button.textContent;
 }
 
+function appendDecimal() {
+    // would cause problems in parsing so we'll wait.
+}
+
+function keyPress(event) {
+    event.preventDefault();
+    if (event.key.match(/[0-9]/g)) {
+        let loopHole = {
+            textContent: event.key
+        };
+        appendNum(loopHole);
+    } else if (event.key.match(/[=]/g)) operateParse();
+
+    if (event.shiftKey) {
+        if (event.key.match(/[+*]/g)) addOperator(event.key);
+    }
+}
+
 function deleteRecent() {
     largeText.textContent = largeText.textContent.slice(0, -1);
     if (largeText.textContent == '') largeText.textContent += "0";
@@ -24,11 +42,13 @@ function clearText() {
 }
 
 function addOperator(button) {
+    if (largeText.textContent == "No.") return;
     // if operator is already here, operate
     if (smallText.textContent.match(/[+-/*]/g) && !smallText.textContent.match(/[=]/g)) {
         let tempOp = operateParse();
 
         smallText.textContent = largeText.textContent + ` ${tempOp} `;
+        console.log(tempOp);
     } else {
         smallText.textContent = largeText.textContent + ` ${button.textContent} `;
         replace = true;
@@ -50,12 +70,15 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b == 0) {
+        replace = true;
         return "No.";
     }
     return a/b;
 }
 
 function operateParse() {
+    if (smallText.textContent.match(/[=]/g) || smallText.textContent == '') return;
+
     let tempArr = smallText.textContent.split("").filter((num) => num != " ").join("");
     let firstNum = tempArr.split(/[d+]/g)[0];
     let tempOp = tempArr[tempArr.length-1];
@@ -67,7 +90,9 @@ function operateParse() {
     smallText.textContent += ` ${secNum} =`;
     largeText.textContent = result;
 
-    return tempArr[1];
+    replace = true;
+
+    return tempOp;
 }
 
 function operate(op, a, b) {
