87e5e320e0a07a1e84470d8b7db05b0f4d9921f5
TheCheeseMeister <ssomething511@gmail.com>
2024-07-12 23:12:40 -0400

    Added most of button functionality for calculator. All operators work as well as clear and delete. Any number will work with it.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,41 @@
+const buttonsNum = document.querySelector("#buttonNum");
+
+const smallText = document.querySelector("#small-text");
+const largeText = document.querySelector("#large-text");
+
+let replace = false;
+
+function appendNum(button) {
+    if (largeText.textContent == "0" || replace) {
+        largeText.textContent = button.textContent;
+        replace = false;
+    }
+    else largeText.textContent += button.textContent;
+}
+
+function deleteRecent() {
+    largeText.textContent = largeText.textContent.slice(0, -1);
+    if (largeText.textContent == '') largeText.textContent += "0";
+}
+
+function clearText() {
+    largeText.textContent = "0";
+    smallText.textContent = "";
+}
+
+function addOperator(button) {
+    // if operator is already here, operate
+    if (smallText.textContent.match(/[+-/*]/g) && !smallText.textContent.match(/[=]/g)) {
+        let tempOp = operateParse();
+
+        smallText.textContent = largeText.textContent + ` ${tempOp} `;
+    } else {
+        smallText.textContent = largeText.textContent + ` ${button.textContent} `;
+        replace = true;
+    }
+}
+
+// Calculator math functionality
 function add(a, b) {
     return a+b;
 }
@@ -11,9 +49,39 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b == 0) {
+        return "No.";
+    }
     return a/b;
 }
 
-let firstNum = null;
-let operator = null;
-let secondNum = null;
+function operateParse() {
+    let tempArr = smallText.textContent.split("").filter((num) => num != " ").join("");
+    let firstNum = tempArr.split(/[d+]/g)[0];
+    let tempOp = tempArr[tempArr.length-1];
+
+    let secNum = parseInt(largeText.textContent);
+
+    let result = operate(tempOp, parseInt(firstNum), secNum);
+
+    smallText.textContent += ` ${secNum} =`;
+    largeText.textContent = result;
+
+    return tempArr[1];
+}
+
+function operate(op, a, b) {
+    let num1 = parseInt(a);
+    let num2 = parseInt(b);
+
+    switch(op) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
